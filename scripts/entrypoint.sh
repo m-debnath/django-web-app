a97a98fc378e8304e06e26f71f3312f56008e763
Mukul Debnath <debnath.mukul@gmail.com>
2022-07-13 03:34:03 +0530

    Cleaning up

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,6 +6,6 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-# celery -A testrail_import_app worker -l info --detach
-# daphne -b 0.0.0.0 -p 8000 testrail_import_app.asgi:application
+# celery -A core worker -l info --detach
+# daphne -b 0.0.0.0 -p 8000 core.asgi:application
 python3 manage.py runserver 0.0.0.0:8000
